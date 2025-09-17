72e7b1f11f7794b0dc0113f41233425d406c6903
Logan Moore <lmooore@gmail.com>
2025-09-17 09:30:11 -0500

    switch to mmap, disable some stuff for now

--- initial-kv/kv.c
+++ initial-kv/kv.c
@@ -1,7 +1,11 @@
+#include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #define FILENAME "db.txt"
 #define LINE_LIM 1023
@@ -9,12 +13,35 @@
 
 int main(int argc, char** argv) {
   int i, line, key, result, linekey, foundline;
-  FILE* fd = NULL;
+  int fd = open(FILENAME, O_RDWR);
+  struct stat sb;
+
+  if (fd == -1) {
+    printf("couldn't open file '%s': error code %d\n", FILENAME, errno);
+    return 1;
+  }
+
+  if (fstat(fd, &sb) == -1) {
+    printf("couldn't stat file '%s': error code %d\n", FILENAME, errno);
+    return 1;
+  }
+
+  void *fptr = mmap(
+    NULL,
+    sb.st_size, // open whole file
+    PROT_WRITE,
+    MAP_SHARED,
+    fd,
+    0
+  );
+
+  assert(fptr != (void *) -1);
+
   char command;
   char *command_raw, *data, *linedata;
   command_raw = (char*) malloc(sizeof(char[2]));
-  data        = (char*) malloc(sizeof(char[1024]));
-  linedata    = (char*) malloc(sizeof(char[LINE_LIM-KEY_LEN]));
+  data        = (char*) malloc(sizeof(char[LINE_LIM+1]));
+  linedata    = (char*) malloc(sizeof(char[LINE_LIM-KEY_LEN+1]));
 
   for (i = 0; i < argc-1; i++) {
     command_raw[0] = '\0';
@@ -29,16 +56,6 @@ int main(int argc, char** argv) {
 
     command = command_raw[0];
 
-    if (fd == NULL)
-      fd = fopen(FILENAME, "r+");
-    else
-      fd = freopen(NULL, "r+", fd);
-
-    if (fd == NULL) {
-      printf("couldn't open file '%s': error code %d\n", FILENAME, errno);
-      return 1;
-    }
-
     switch (command) {
       case 'a':
         break; // TODO implement all (list all entries)
@@ -49,34 +66,34 @@ int main(int argc, char** argv) {
       case 'p': {
         foundline = -1;
         line = 0;
-        while ((result = fscanf(fd, "%i=%s\n", &linekey, linedata)) > 0) {
-          if (linekey == key) {
-            foundline = line;
-            break;
-          }
-          line++;
-        }
+        // while ((result = fscanf(fd, "%i=%s\n", &linekey, linedata)) > 0) {
+          // if (linekey == key) {
+            // foundline = line;
+            // break;
+          // }
+          // line++;
+        // }
 
-        if (foundline < 0) {
-          fd = freopen(FILENAME, "a", fd);
-          fprintf(fd, "%d=%s\n", key, data);
-        } else {
+        // if (foundline < 0) {
+          // fd = freopen(FILENAME, "a", fd);
+          // fprintf(fd, "%d=%s\n", key, data);
+        // } else {
           // TODO implement replacement put
-        }
+        // }
 
         break;
       }
       case 'g': {
         foundline = -1;
         line = 0;
-        while ((result = fscanf(fd, "%i=%s\n", &linekey, linedata)) > 0) {
-          if (linekey == key) {
-            foundline = line;
-            printf("%d=%s\n", linekey, linedata);
-            break;
-          }
-          line++;
-        }
+        // while ((result = fscanf(fd, "%i=%s\n", &linekey, linedata)) > 0) {
+          // if (linekey == key) {
+            // foundline = line;
+            // printf("%d=%s\n", linekey, linedata);
+            // break;
+          // }
+          // line++;
+        // }
 
         if (foundline < 0)
           printf("%d: key does not exist\n", key);
